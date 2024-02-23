4c5cc9bb1b4b2fd6e09db440eb77c301004c1d6b
tamziy <114886626+tamziy@users.noreply.github.com>
2024-02-23 14:32:23 -0600

    Created DrawLine Class

--- BOIDS/modules/userInteraction.js
+++ BOIDS/modules/userInteraction.js
@@ -19,3 +19,52 @@ export class MouseAttraction {
         return this.isAttractedToMouse ? this.mousePosition : null;
     }
 }
+
+export class DrawLine {
+    constructor() {
+        this.isDrawing = false;
+        this.linePoints = [];
+        this.enableDrawing = false;
+
+        document.getElementById('draw-line').addEventListener('click', ()=> {
+            this.enableDrawing = !this.enableDrawing;
+            this.linePoints = [];
+
+            if(!this.enableDrawing) {
+                this.finishDrawing();
+            }
+        });
+
+        canvas.addEventListener('mousedown', (e) => {
+            if (!this.enableDrawing) return;
+            this.isDrawing = true;
+            this.linePoints.push({ x: e.clientX, y: e.clientY });
+        });
+
+        canvas.addEventListener('mousemove', (e) => {
+            if (!this.enableDrawing || !this.isDrawing) return;
+            this.linePoints.push({ x: e.clientX, y: e.clientY });
+            this.drawPoint(e.clientX, e.clientY);
+        });
+
+        canvas.addEventListener('mouseup', () => {
+            this.isDrawing = false;
+        });
+    }
+
+    drawPoint(x, y) {
+        ctx.fillStyle = 'black'; 
+        ctx.beginPath();
+        ctx.arc(x, y, 2, 0, Math.PI * 2); // Draw small circle to represent the line point
+        ctx.fill();
+    }
+
+    finishDrawing() {
+        //TODO: Implement Logic for boids to follow
+    }
+
+    getLinePoints() {
+        return this.enableDrawing ? this.linePoints : [];
+    }
+
+}
